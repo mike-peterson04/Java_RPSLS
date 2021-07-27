0c826cdfa4ddde8ed45e2cb10231faf59e7221d1
Mike Peterson <mpeterson2004@gmail.com>
2021-07-27 16:19:13 -0600

    added skeleton of planned classes

--- rpsls/src/main/java/rpsls/Player.java
+++ rpsls/src/main/java/rpsls/Player.java
@@ -5,19 +5,10 @@
  */
 package rpsls;
 
-
 /**
  *
  * @author Admin
  */
-public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hello World");
-    }
+public class Player {
     
 }
