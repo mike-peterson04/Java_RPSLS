70ebcdeb8bea6c8bf47bba119f02d7cafe0c6c07
Mike Peterson <mpeterson2004@gmail.com>
2021-07-28 16:50:57 -0600

    created sign selector for human player

--- rpsls/src/main/java/rpsls/HumanPlayer.java
+++ rpsls/src/main/java/rpsls/HumanPlayer.java
@@ -5,19 +5,56 @@
  */
 package rpsls;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Admin
  */
 public class HumanPlayer extends Player {
+    Scanner keyboard;
     
     public HumanPlayer(String PlayerName){
-       super(PlayerName);  
+       super(PlayerName);
+       keyboard = new Scanner(System.in);
     }
     
     @Override
     public String SelectSign(){
         String gesture = "ERROR";
+        OUTER:
+        while (true) {
+            System.out.println("Please select a gesture");
+            System.out.println("1 for Rock");
+            System.out.println("2 for Paper");
+            System.out.println("3 for Scissors");
+            System.out.println("4 for Lizard");
+            System.out.println("5 for Spock");
+            String gestureIndex = keyboard.nextLine();
+            if (null != gestureIndex) {
+                switch (gestureIndex) {
+                    case "1":
+                        gesture = "ROCK";
+                        break OUTER;
+                    case "2":
+                        gesture = "PAPER";
+                        break OUTER;
+                    case "3":
+                        gesture = "SCISSORS";
+                        break OUTER;
+                    case "4":
+                        gesture = "LIZARD";
+                        break OUTER;
+                    case "5":
+                        gesture = "SPOCK";
+                        break OUTER;
+                    default:
+                        System.out.println("Please enter a number between 1-5");
+                        break;
+                }
+            }
+        }
+        
         return gesture;
     }
     
